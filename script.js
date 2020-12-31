1282849b0d68a2eb1aeb0d18173c492a9a1172c6
lvdesignfr <work@lvdesign.com.fr>
2020-12-31 09:52:21 +0100

    version1

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var d = new Date();
+    //amount
+    let p = document.getElementById("principal").value;
+    // years
+    let y = document.getElementById("years").value;
+    // rate
+    let r = document.getElementById("rate").value;
     
+    let deposit = p;
+    let interest = y;
+    let rating = r;
+
+    let calcul = (p * r) / 100;
+    let year = d.getFullYear();
+    let annee = year + y;
+
+    console.log( 'deposit :'+ deposit, 'interest :' +interest, 'calcul:' +calcul, 'year:'+ annee, 'rating :' +rating, );
+
+    return p;
+    
+
 }
         
